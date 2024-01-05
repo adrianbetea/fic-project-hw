4d8c289484885115dd2ff29f94e2cd0cc3403b1e
adrianbetea <beteaadrian03gmail.com>
2024-01-05 20:23:55 +0200

    sensor added

--- food_counter.v
+++ food_counter.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module food_counter(input clock,input reset,input enable, input count_reset,  output switch_f_c, output [3:0] count_out);
+module food_counter(input clock,input reset,input enable, input count_reset, input full_sensor,  output switch_f_c, output [3:0] count_out);
 
 
   reg [3:0] count_reg, count_next;
@@ -7,7 +7,7 @@ module food_counter(input clock,input reset,input enable, input count_reset,  ou
   localparam food_time = 10;
 
     always @(posedge clock or posedge reset) begin
-        if(reset || count_reset) begin
+        if(reset || count_reset || full_sensor) begin
             count_reg <= 'b0;
             switch_f_c_reg<='b0;
         end else begin
@@ -19,13 +19,14 @@ module food_counter(input clock,input reset,input enable, input count_reset,  ou
     always @* begin
         count_next = count_reg;
         switch_f_c_next = switch_f_c_reg;
-        if(enable && !count_reset) begin
+        if(enable && !count_reset && !full_sensor) begin
             if(count_reg <= (food_time - 1) ) begin
                 switch_f_c_next = 1'b1;
             end
             // aici se reseteaza contorul
             if(count_reg == (food_time)) begin
                 switch_f_c_next = 1'b0;
+                count_next = 0;
                 // problema apare aici din cauza lui count_reg care nu se reseteaza (nici nu pot sa-l resetez eu ca numara nonstop)
             end else begin
                 count_next = count_reg+1'b1;
@@ -40,17 +41,18 @@ endmodule
 
 `timescale 1ns/1ns
 module tb_food_counter();
-  reg clock, reset, enable, count_reset;
+  reg clock, reset, enable, count_reset, full_sensor;
   wire switch_f_c;
   wire [3:0]count_out;
   
-  food_counter food_counter_instance(.clock(clock),.reset(reset),.enable(enable),.count_reset(count_reset),.switch_f_c(switch_f_c), .count_out(count_out));
+  food_counter food_counter_instance(.clock(clock),.reset(reset),.enable(enable),.count_reset(count_reset), .full_sensor(full_sensor), .switch_f_c(switch_f_c), .count_out(count_out));
   
   initial begin
     clock = 0;
     forever #2 clock = !clock;
   end
   initial begin
+    full_sensor = 1'b0;
     reset = 1'b1;
     count_reset = 1'b0;
     enable  = 1'b1;
@@ -59,7 +61,10 @@ module tb_food_counter();
     count_reset = 1'b1;
     #30
     count_reset = 1'b0;
+    enable = 1'b1;
     #200;
+    full_sensor = 1'b1;
+    #400;
     $finish();
   end
   initial begin
--- interval_counter.v
+++ interval_counter.v
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
-module interval_counter(input clock,input reset,input enable,input interval_reset, input [15:0]interval, 
+module interval_counter(input clock,input reset,input enable,input interval_reset, input full_sensor, input [15:0]interval, 
                       output switch_i_c, output [20:0] count_out);
     // intervalul o sa fie dat in minute, deci il inmultim cu 60
   reg [20:0] count_reg, count_next;
@@ -7,7 +7,7 @@ module interval_counter(input clock,input reset,input enable,input interval_rese
   reg [20:0]interval_seconds_reg;
 
     always @(posedge clock or posedge reset) begin
-        if(reset || interval_reset) begin
+        if(reset || interval_reset || full_sensor) begin
             count_reg <= 'b0;
             switch_i_c_reg<='b0;
             interval_seconds_reg<=60*interval;
@@ -20,7 +20,7 @@ module interval_counter(input clock,input reset,input enable,input interval_rese
     always @* begin
         count_next = count_reg;
         switch_i_c_next = switch_i_c_reg;
-        if(enable && !interval_reset) begin
+        if(enable && !interval_reset && !full_sensor) begin
             if(count_reg <= (interval_seconds_reg - 2) && count_reg >= (interval_seconds_reg - 6)) begin
                 switch_i_c_next = 1'b1;
             end
@@ -41,19 +41,20 @@ endmodule
 
 `timescale 1ns/1ns
 module tb_interval_counter();
-  reg clock, reset, enable, interval_reset;
+  reg clock, reset, enable, interval_reset, full_sensor;
   reg [15:0]interval;
   wire switch_i_c;
   wire [20:0]count_out;
   
-  interval_counter interval_counter_instance(.clock(clock),.reset(reset),.enable(enable),.interval_reset(interval_reset) ,.interval(interval),.switch_i_c(switch_i_c), .count_out(count_out));
+  interval_counter interval_counter_instance(.clock(clock),.reset(reset),.enable(enable),.interval_reset(interval_reset), .full_sensor(full_sensor) ,.interval(interval),.switch_i_c(switch_i_c), .count_out(count_out));
   
   initial begin
     clock = 0;
     forever #2 clock = !clock;
   end
   initial begin
-    interval=5'd10;
+    full_sensor = 1'b0;
+    interval=5'd3;
     reset = 1'b1;
     interval_reset = 1'b0;
     enable  = 1'b1;
@@ -64,6 +65,10 @@ module tb_interval_counter();
     interval_reset = 1'b0;
     #10;
     enable = 1'b1;
+    #1000;
+    full_sensor = 1'b1;
+    #1000;
+    full_sensor = 1'b0;
     #20000;
     $finish();
   end
